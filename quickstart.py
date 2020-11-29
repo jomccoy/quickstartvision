b61065520d6ac33b1f1efd25c4aea73cfcde0ca8
Josh <Josh@MININT-JJRLKIE.northamerica.corp.microsoft.com>
2020-11-29 07:27:10 -0500

    initial commit for congnitive services

---
+++ quickstart.py
@@ -0,0 +1,132 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+
+subscription_key = "fca73e401a8241398ec111b93b340dae"
+endpoint = "https://jomccoyvision.cognitiveservices.azure.com/"
+
+#Authenticate client
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+#Analyze an image
+#remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
+remote_image_url = "https://ontheflightpath.files.wordpress.com/2014/05/statue-of-liberty.jpg"
+#remote_image_url = "https://cvs33.files.wordpress.com/2019/01/red-fox-winter-south-china-4-hal-winters-1.jpg"
+
+'''
+#Get Image Description
+Describe an Image - remote
+This example describes the contents of an image with the confidence score.
+'''
+print("===== Describe an image - remote =====")
+# Call API
+description_results = computervision_client.describe_image(remote_image_url )
+
+# Get the captions (descriptions) from the response, with confidence level
+print("Description of remote image: ")
+if (len(description_results.captions) == 0):
+    print("No description detected.")
+else:
+    for caption in description_results.captions:
+        print("'{}' with confidence {:.2f}%".format(caption.text, caption.confidence * 100))
+
+
+'''
+#Get Image Category
+Categorize an Image - remote
+This example extracts (general) categories from a remote image with a confidence score.
+'''
+print("===== Categorize an image - remote =====")
+# Select the visual feature(s) you want.
+remote_image_features = ["categories"]
+# Call API with URL and features
+categorize_results_remote = computervision_client.analyze_image(remote_image_url , remote_image_features)
+
+# Print results with confidence score
+print("Categories from remote image: ")
+if (len(categorize_results_remote.categories) == 0):
+    print("No categories detected.")
+else:
+    for category in categorize_results_remote.categories:
+        print("'{}' with confidence {:.2f}%".format(category.name, category.score * 100))
+
+
+
+#Get Image Tags
+'''
+Tag an Image - remote
+This example returns a tag (key word) for each thing in the image.
+'''
+print("===== Tag an image - remote =====")
+# Call API with remote image
+tags_result_remote = computervision_client.tag_image(remote_image_url )
+
+# Print results with confidence score
+print("Tags in the remote image: ")
+if (len(tags_result_remote.tags) == 0):
+    print("No tags detected.")
+else:
+    for tag in tags_result_remote.tags:
+        print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
+
+# Call API with content type (landmarks) and URL
+detect_domain_results_landmarks = computervision_client.analyze_image_by_domain("landmarks", remote_image_url)
+print()
+
+print("Landmarks in the remote image:")
+if len(detect_domain_results_landmarks.result["landmarks"]) == 0:
+    print("No landmarks detected.")
+else:
+    for landmark in detect_domain_results_landmarks.result["landmarks"]:
+        print(landmark["name"])
+
+
+        
+        '''
+Detect Objects - remote
+This example detects different kinds of objects with bounding boxes in a remote image.
+'''
+print("===== Detect Objects - remote =====")
+# Get URL image with different objects
+remote_image_url_objects = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/objects.jpg"
+# Call API with URL
+detect_objects_results_remote = computervision_client.detect_objects(remote_image_url_objects)
+
+# Print detected objects results with bounding boxes
+print("Detecting objects in remote image:")
+if len(detect_objects_results_remote.objects) == 0:
+    print("No objects detected.")
+else:
+    for object in detect_objects_results_remote.objects:
+        print("object at location {}, {}, {}, {}".format( \
+        object.rectangle.x, object.rectangle.x + object.rectangle.w, \
+        object.rectangle.y, object.rectangle.y + object.rectangle.h))
+
+
+'''
+Detect Brands - remote
+This example detects common brands like logos and puts a bounding box around them.
+'''
+print("===== Detect Brands - remote =====")
+# Get a URL with a brand logo
+remote_image_url = "https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/images/gray-shirt-logo.jpg"
+# Select the visual feature(s) you want
+remote_image_features = ["brands"]
+# Call API with URL and features
+detect_brands_results_remote = computervision_client.analyze_image(remote_image_url, remote_image_features)
+
+print("Detecting brands in remote image: ")
+if len(detect_brands_results_remote.brands) == 0:
+    print("No brands detected.")
+else:
+    for brand in detect_brands_results_remote.brands:
+        print("'{}' brand detected with confidence {:.1f}% at location {}, {}, {}, {}".format( \
+        brand.name, brand.confidence * 100, brand.rectangle.x, brand.rectangle.x + brand.rectangle.w, \
+        brand.rectangle.y, brand.rectangle.y + brand.rectangle.h))
